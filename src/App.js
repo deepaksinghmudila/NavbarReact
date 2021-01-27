1503cf01098222501a145845fddc86321874a968
deepaksinghmudila <deepaksinghmudila21@gmail.com>
2021-01-27 13:57:28 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./header/Header";
+import "./style.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <h3 style={{ padding: "100px" }}>Thanks For Reading!</h3>
     </div>
   );
 }
-
-export default App;
